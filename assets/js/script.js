738f730be9e83add28a5efd6a64c108bea3607e1
ederxx <edercruz.x@gmail.com>
2022-01-10 12:18:25 -0300

    atulizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,27 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0;
 
+
 function increment(){
     if(currentNumber < 10)
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber>0) {
+        currentNumberWrapper.style.color = "black";
+    }
 }
 function decrement(){
     if(currentNumber > -10){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
-    if(currentNumber>0){
-    currentNumber.addEventListener("click",addEventos);
-    }
-
-}
-function addEventos(){
-    document.getElementById(currentNumber).style.color="#ffff00";
-}
-
+if (currentNumber<0) {
+    currentNumberWrapper.style.color = "red";
+} else {
+    currentNumberWrapper.style.color = "black";
+} 
+    }    
  
+       
 }
+
+
